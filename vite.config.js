82c64d7e03106465e7e444be2fdc02d6d4421149
jixiaojiao <cc_byland@163.com>
2023-11-25 11:20:04 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'
 
 export default defineConfig({
+  base: "universe",
   plugins: [
     vue(), ElementPlus()
   ],
