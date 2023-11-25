34e84b52ca00a9bb388182389ba32040496d8232
jixiaojiao <cc_byland@163.com>
2023-11-25 21:06:38 +0800

    1

--- src/modules/sprite.js
+++ src/modules/sprite.js
@@ -17,10 +17,10 @@ export function add(SCENE) {
   const vertices = [];
   const spriteCount = globalConfig.spriteCount
   for (let i = 0; i < spriteCount; i++) {
-    // 在区间 [-6000,6000] 内随机一个浮点数。
-    const x = THREE.MathUtils.randFloatSpread(spriteCount / 2);
-    const y = THREE.MathUtils.randFloatSpread(spriteCount / 2);
-    const z = THREE.MathUtils.randFloatSpread(spriteCount / 2);
+
+    const x = THREE.MathUtils.randFloatSpread(6000);
+    const y = THREE.MathUtils.randFloatSpread(6000);
+    const z = THREE.MathUtils.randFloatSpread(6000);
     vertices.push(x, y, z);
   }
 
